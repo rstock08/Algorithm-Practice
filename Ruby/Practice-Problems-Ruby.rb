2e31ffd7d16609efafa30f9a54d9134f4f9c72f6
ReedS <ReedStock1992@gmail.com>
2019-04-08 23:14:42 -0700

    - Prep done to begin working on practice problems in C# and Ruby - Built directory out for several languages

---
+++ Ruby/Practice-Problems-Ruby.rb
@@ -0,0 +1,7 @@
+# Practice white board questions with Ruby.
+# Project Author: Reed Stock
+# Source: https://simpleprogrammer.com/programming-interview-questions/
+
+# Question 1:
+# How do you find the missing number in a given integer array of 1 to 100?
+# my_simple_class.rb
