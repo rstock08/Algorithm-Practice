ed41aa1d6f18b6d8583787b80317dd89eb088d6c
ReedS <ReedStock1992@gmail.com>
2019-05-05 18:21:27 -0700

    - Continued work on HackerRank questions    - Problem(HackerRank) : https://www.hackerrank.com/challenges/time-conversion/problem

--- CS/CS/HackerRankAlgoWarmup.cs
+++ CS/CS/HackerRankAlgoWarmup.cs
@@ -30,7 +30,54 @@ namespace CS
             return abScore;
         }
 
-        //Problem(HackerRank) : https://www.hackerrank.com/challenges/compare-the-triplets/problem
+        //Problem(HackerRank) : https://www.hackerrank.com/challenges/time-conversion/problem
+        static string timeConversion(string s)
+        {
+            string tempT = "";
+            int tempVal1 = 0, tempVal2 = 0;
 
+            if (s.Contains("AM"))
+            {
+
+                s = s.Replace("AM", "");
+
+                if (s.Substring(0, 2).Contains("12"))
+                {
+                    s = s.Replace(s.Substring(0, 2), "00");
+                }
+            }
+            else if (s.Contains("PM"))
+            {
+
+                s = s.Replace("PM", "");
+
+                if (s.Substring(0, 2).Contains("12"))
+                {
+                    //String stays the same
+                }
+                else
+                {
+                    tempT = s.Substring(0, 2);
+                    tempVal1 = Convert.ToInt32(tempT);
+                    tempVal2 = tempVal1 + 12;
+
+                    if (tempVal1 < 10)
+                    {
+                        s = s.Replace(s.Substring(0, 2), tempVal2.ToString());
+                    }
+                    else
+                    {
+                        s = s.Replace(tempVal1.ToString(), tempVal2.ToString());
+                    }
+                }
+            }
+
+            // Debug
+            //Console.WriteLine(tempT);
+            //Console.WriteLine(tempVal1);
+            //Console.WriteLine(tempVal2);
+
+            return s;
+        }
     }
 }
