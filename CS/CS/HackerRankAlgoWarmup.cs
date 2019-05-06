7621a30193b44f05da80051b3a9f40ea4fd05277
ReedS <ReedStock1992@gmail.com>
2019-05-05 18:18:41 -0700

    - Practice HackerRank problems   - Problem (HackerRank): https://www.hackerrank.com/challenges/compare-the-triplets/problem

---
+++ CS/CS/HackerRankAlgoWarmup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS
+{
+    
+    // Complete the compareTriplets function below.
+    class HackerRankAlgoWarmup
+    {
+
+        // Problem (HackerRank): https://www.hackerrank.com/challenges/compare-the-triplets/problem
+        public static List<int> CompareTriplets(List<int> a, List<int> b)
+        {
+            List<int> abScore = new List<int>() { 0, 0 };
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (a[i] > b[i])
+                {
+                    abScore[0] += 1;
+                }
+                else if (a[i] < b[i])
+                {
+                    abScore[1] += 1;
+                }
+            }
+            return abScore;
+        }
+
+        //Problem(HackerRank) : https://www.hackerrank.com/challenges/compare-the-triplets/problem
+
+    }
+}
